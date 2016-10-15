eb3ddff4c2e8383dc63dbfb76e24c965c12deb3f
Ohad Kravchick <myok12@yahoo.com>
2016-10-15 09:41:10 -0700

    second exerice description

---
+++ ex2/ex2.js
@@ -0,0 +1,10 @@
+/*
+ * Write a function that accepts a string and a number (n) and returns the index of the n-th comma in that string.
+ * If there's no such comma, the function should return -1. If the n argument is invalid, the function should throw an Error.
+ * For example:
+ *     fn("hello,world,this,is,a,great,day", 3) === 16
+ *     fn("hello,world,this,is,a,great,day", 4) === 19
+ *     fn(",,,", 1) === 0
+ *     fn(",,,", 4) === -1
+ *     fn("hel,lowo,rld", 0) - Throws
+ */
