bdc2e7c4694fa83ad988013e31ee3545e5503d94
Ohad Kravchick <myok12@yahoo.com>
2016-10-15 09:36:29 -0700

    first exercise description

---
+++ ex1/ex1.js
@@ -0,0 +1,8 @@
+/*
+ * Write a function that accepts a string and returns the index of the third comma in that string.
+ * If there's no such comma, the function should return -1.
+ * For example:
+ *     fn("hello,world,this,is,a,great,day") === 16
+ *     fn(",,,") === 2
+ *     fn("hel,lowo,rld") === -1
+ */
