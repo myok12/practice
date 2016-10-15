037b8656042376dbf7df8a902091f1607b05ea4a
Zuriel Yahav <zyahav@gmail.com>
2016-10-16 01:48:29 +0300

    Manually copied old function

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -6,3 +6,25 @@
  *     fn(",,,") === 2
  *     fn("hel,lowo,rld") === -1
  */
+
+/////////////////////////////////////////////////////////////////////////////
+// getThirdCommaPosition accepts a string and returns the index of the third comma in that string /  no comma return -1
+function getThirdCommaPosition(str,searcVal) {
+
+	var i, position = -1;
+
+	for (i = 0; i < 3; i++) {
+		position = str.indexOf(searcVal, position+1);
+		if (position == -1) { 
+			break;
+		}
+	}
+	return position;
+}
+
+//var str = 'hello,world,this,is,a,great,day'// === 16
+//var str = ',,,';// === 2
+var str = 'hel,lowo,rld';// === -1
+
+console.log(getThirdCommaPosition(str, ','));
+////////////////////////////////////////////////////////////////////////////
