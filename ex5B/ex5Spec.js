657079c3ce96eeefb682af9c78434d8f677e1877
Zuriel Yahav <zyahav@gmail.com>
2016-11-25 08:50:29 +0200

    s/characte/character/

--- ex5B/ex5Spec.js
+++ ex5B/ex5Spec.js
@@ -1,16 +1,3 @@
-/**
- * Write a function that given a string, returns the first character that
- * doesn't repeat in the whole string. Otherwise (all characters repeat),
- * should return null.
- *
- * For example:
- * ex5.firstUniqueChar("abcdedcba") === "e"
- * ex5.firstUniqueChar("abcdeedcba") === null
- * ex5.firstUniqueChar("abababcab") === "c"
- * ex5.firstUniqueChar("aaaaaaaaa") === null
- * ex5.firstUniqueChar("a") === "a"
- */
-
 var ex5 = require('./ex5.js');
 
 describe("firstUniqueChar", function() {
@@ -18,19 +5,19 @@ describe("firstUniqueChar", function() {
 		expect(ex5.firstUniqueChar("abcdedcba")).toBe("e");
 	});
 
-	it("return null, we do not have a uniq char on string", function() {
+	it("return null when we only have one [non-unique] char", function() {
 		expect(ex5.firstUniqueChar("abcdeedcba")).toBeNull();
 	});
 
-	it("finds another unique character in the strin", function() {
+	it("finds another unique character in the string", function() {
 		expect(ex5.firstUniqueChar("abababcab")).toBe("c");
 	});
 
-	it("return null, we do not have a uniq char on strin", function() {
+	it("return null, all chars are same, function() {
 		expect(ex5.firstUniqueChar("aaaaaaaaa")).toBeNull();
 	});
 
-	it("works for a single characte", function() {
+	it("works for a single character", function() {
 		expect(ex5.firstUniqueChar("a")).toBe("a");
 	});
 });
