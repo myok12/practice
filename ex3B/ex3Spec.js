ceb93799a01e3c141be961f52477e54d474bfd11
Zuriel Yahav <zyahav@gmail.com>
2016-12-01 15:50:41 +0200

    replace test > it

--- ex3B/ex3Spec.js
+++ ex3B/ex3Spec.js
@@ -2,15 +2,15 @@ var containsValue = require('./ex3');
 
 describe("containsValue", function() {
 
-	it("should not find the number 8 in array", function() {
+	it("should correctly answer when the value isnt found", function() {
 		expect(containsValue([1,3,5,7,9], 8)).toBeFalsy();
 	});
 
-	it("should find the number 7 in array", function() {
+	it("should find a number when the input is in order", function() {
 		expect(containsValue([1,3,5,7,9], 7)).toBeTruthy();
 	});
 
-	it("should find the number 7 in array", function() {
+	it("should find a number when the input is not in order", function() {
 		expect(containsValue([9,7,5,3,1], 7)).toBeTruthy();
 	});
 });
