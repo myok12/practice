65d40549f3c2c4092c75cc662b687a2ae003c2f3
Zuriel Yahav <zyahav@gmail.com>
2016-12-01 12:34:26 +0200

    add more tests

--- ex4B/ex4.js
+++ ex4B/ex4.js
@@ -8,12 +8,28 @@
  */
 function toNumber(str) {
 
-	var tmpStr = '';
+	var tmpStr = '', i = 0, dotRepeat = null;
 
-	for (var i = 0; i < str.length; i++) {
+	// Negative sine can appear only at the start of string and followed by number
+	if (str.slice(i, i + 1) === '-' && isNumber(str.slice(i+1, i + 2))) {
+	
+		tmpStr += str.slice(i, i + 2);  
+		i = 2;
+	}
 
-		if (str.slice(i, i + 1) >= 0 && str.slice(i, i + 1) <= 9) {
+	// Dot cannot appear at the start or and of string
+	if (str[0] === '.' || str[str.length - 1]=== '.') {
+		return null;
+	}
 
+	for (;i < str.length; i++) {
+
+		if (isNumber(str.slice(i, i + 1)) ||  str.slice(i, i + 1) === '.' && !dotRepeat) {
+ 
+			// Ziro tolerants for dots
+			if (str.slice(i, i + 1) === '.') {
+				dotRepeat = true;
+			} 
 			tmpStr += str.slice(i, i + 1);  
 
 		} else {
@@ -24,6 +40,17 @@ function toNumber(str) {
 	}
 
 	return Number(tmpStr);
+}
+
+
+function isNumber(val) {
+
+	if (val >= 0 && val <= 9) {
+		return true;
+	} else {
+		return false;
+	}
+
 }
 
 module.exports = {toNumber: toNumber}
