02295649d6bd9e89c794c0191e3cc1379e06d263
Zuriel Yahav <zyahav@gmail.com>
2016-12-01 15:43:47 +0200

    typo shoud to should

--- ex3B/ex3Spec.js
+++ ex3B/ex3Spec.js
@@ -2,15 +2,15 @@ var containsValue = require('./ex3');
 
 describe("containsValue", function() {
 
-	it("shoud not find the number 8 in array", function() {
+	it("should not find the number 8 in array", function() {
 		expect(containsValue([1,3,5,7,9], 8)).toBeFalsy();
 	});
 
-	it("shoud find the number 7 in array", function() {
+	it("should find the number 7 in array", function() {
 		expect(containsValue([1,3,5,7,9], 7)).toBeTruthy();
 	});
 
-	it("shoud find the number 7 in array", function() {
+	it("should find the number 7 in array", function() {
 		expect(containsValue([9,7,5,3,1], 7)).toBeTruthy();
 	});
 });
