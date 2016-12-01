1ac33f0d615d70ef4cdd726b7f0cf0635635b011
Zuriel Yahav <zyahav@gmail.com>
2016-12-01 12:53:44 +0200

    typo and break lines

--- ex1/ex1Spec.js
+++ ex1/ex1Spec.js
@@ -2,16 +2,18 @@ var ex1 = require("./ex1");
 
 describe("getThirdCommaIndex", function() {
 
-
 	it("returns -1 when no third comma", function() {
-		expect(ex1.getThirdCommaIndex('hel,lowo,rld', ',')).toBe(-1);
+		expect(ex1.getThirdCommaIndex('hel,lowo,rld', ','))
+		.toBe(-1);
 	});
 
-	it("finds a comma in a regular sentenc", function() {
-		expect(ex1.getThirdCommaIndex('hello,world,this,is,a,great,day', ',')).toBe(16);
+	it("finds a comma in a regular sentence", function() {
+		expect(ex1.getThirdCommaIndex('hello,world,this,is,a,great,day', ','))
+		.toBe(16);
 	});
 
 	it("works even when there are no characters beside commas", function() {
-		expect(ex1.getThirdCommaIndex(',,,', ',')).toBe(2);
+		expect(ex1.getThirdCommaIndex(',,,', ','))
+		.toBe(2);
 	});
 });
