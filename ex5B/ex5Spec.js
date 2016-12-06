dbd61247b7897b5a15d218e387e1ab4c9168fa42
Zuriel Yahav <zyahav@gmail.com>
2016-12-06 10:24:01 +0200

    new test

--- ex5B/ex5Spec.js
+++ ex5B/ex5Spec.js
@@ -16,6 +16,13 @@ describe("function firstUniqueChar", function() {
 	it("return null, all chars are same", function() {
 		expect(firstUniqueChar("aaaaaaaaa")).toBeNull();
 	});
+	it("works on really long strings", function() {
+		var longString = '';
+		for (i=100;i<100000;i++) {
+			longString += String.fromCharCode(i);
+		}
+		firstUniqueChar(longString);
+	});
 
 	it("works for a single character", function() {
 		expect(firstUniqueChar("a")).toBe("a");
