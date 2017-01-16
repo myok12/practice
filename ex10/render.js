fe4c902ee5a1fe00b176cb885b24961c5b4d1ae9
Zuriel Yahav <zyahav@gmail.com>
2017-01-16 07:48:35 +0200

    style render

--- ex10/render.js
+++ ex10/render.js
@@ -3,11 +3,15 @@ define(function() {
   // otherwise i will render "data", (show suggestions)
   return {
     showSuggestions: function(data) {
-      $( 'li' ).remove();
-      for (var i = 0; i < data.length; ++i) {
-        $( 'ol' ).append( '<li>' + data[i] + '</li>');
-      }
-      $( 'ol' ).removeClass( "hide" );
+      if (data.length >= 1) {
+        $( 'li' ).remove();
+        for (var i = 0; i < data.length; ++i) {
+          $( 'ol' ).append( '<li>' + data[i] + '</li>');
+        }
+        $( 'ol' ).removeClass( "hide" );
+       } else {
+        $( 'ol' ).addClass( "hide" );
+       }
     },
     hideSuggestions: function() {
       $( 'ol' ).addClass( "hide" );
