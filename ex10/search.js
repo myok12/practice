bd4ef01edec240cb71f705f2297fc2669590a60e
Zuriel Yahav <zyahav@gmail.com>
2017-01-16 09:29:12 +0200

    call hideSggestions from getSuggestion

--- ex10/search.js
+++ ex10/search.js
@@ -1,26 +1,20 @@
 var search = {
   toggleSuggestions: _.debounce(function() {
-      if ($( '.input' ).val() === '') {
-        // User deleted all input so we hide suggestions
-        search.hideSuggestions();
-      } else {
-        // User need same suggestions so lets get same and show it
-        var query = $( '.input' ).val();
-        search.getSuggestions(query , search.showSuggestions);
-      }
-    }, 500),
-  showSuggestions: function(data) {
-    // Call me with no data to hide suggestion box
-    // otherwise i will render "data", (show suggestions)
-    if (data.length >= 1) {
-      $( 'li' ).remove();
-      for (var i = 0; i < data.length; ++i) {
-        $( 'ol' ).append( '<li>' + data[i] + '</li>');
-      }
-      $( 'ol' ).removeClass( "hide" );
-    } else {
+    if ($( '.input' ).val() === '') {
+      // User deleted all input so we hide suggestions
       search.hideSuggestions();
+    } else {
+      // User need same suggestions so lets get same and show it
+      var query = $( '.input' ).val();
+      search.getSuggestions(query , search.showSuggestions);
     }
+  }, 500) ,
+  showSuggestions: function(data) {
+    $( 'li' ).remove();
+    for (var i = 0; i < data.length; ++i) {
+      $( 'ol' ).append( '<li>' + data[i] + '</li>');
+    }
+    $( 'ol' ).removeClass( "hide" );
   },
   hideSuggestions: function() {
     $( 'ol' ).addClass( "hide" );
@@ -31,7 +25,11 @@ var search = {
       method: 'GET',
       error: function(error) { alert('an error' + error); },
       success: function(data) {
-        callback(JSON.parse(data)); 
+        if (data.length >= 1) {
+          callback(JSON.parse(data)); 
+        } else {
+          search.hideSuggestions();
+        }       
       }
     });
   }
